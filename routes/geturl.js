91d3881230761334079af80373dc771d05886166
wuhoho <wumingho@gmail.com>
2016-12-07 11:54:56 +0800

    update request url

--- routes/geturl.js
+++ routes/geturl.js
@@ -3,7 +3,12 @@ var request = require('request');
 var router = express();
 
 router.post('/',function(req, res, next){
-  res.send('a');
+  var url = req.body.url;
+  request(url, function (error, response, body) {
+  if (!error && response.statusCode == 200) {
+    console.log(body) // Show the HTML for the Google homepage.
+  }
+})
 });
 
 module.exports = router;
