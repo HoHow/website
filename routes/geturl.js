c9cff0318a0928969f244fbe75f785597ef90d40
wuhoho <wumingho@gmail.com>
2016-12-12 11:47:34 +0800

    update geturl.js parse-body

--- routes/geturl.js
+++ routes/geturl.js
@@ -3,7 +3,9 @@ var request = require('request');
 var cheerio = require('cheerio');
 var backdata = require('../controller/backdata');
 var router = express();
-
+var bodyParser = require('body-parser');
+router.use(bodyParser.json()); // for parsing application/json
+router.use(bodyParser.urlencoded({ extended: true }));
 router.post('/',function(req, res, next){
   var url = req.body.url;
   request(url, function (error, response, body) {
